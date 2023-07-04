62b7fc5df2ef04e8dfa8a2d85902dad0e88d5c29
Kirill Khalevin <kirkhal0909@gmail.com>
2023-07-05 02:42:37 +0300

    minus exception days

--- stocks_api/app/models/ticker_value.rb
+++ stocks_api/app/models/ticker_value.rb
@@ -2,5 +2,9 @@ class TickerValue < ApplicationRecord
   belongs_to :ticker
   validates :ticker_id, uniqueness: { scope: :date }
 
+  %i[open low high close].each do |column_name|
+    validates column_name, exclusion: { in: [nil, 0] }
+  end
+
   default_scope { order(date: :desc) }
 end

a6349c609e5eda73ff0a2e63fe1885695fc9e40f
Kirill Khalevin <kirkhal0909@gmail.com>
2023-07-05 09:04:12 +0300

    remove unavailable method from scheduler tasks

--- stocks_api/config/initializers/scheduler.rb
+++ stocks_api/config/initializers/scheduler.rb
@@ -1,9 +1,4 @@
 if defined?(Rails::Server)
-  def puts_and_call(service)
-    puts " [SCHEDULER] #{serivce}.call"
-    service.call
-  end
-
   scheduler = Rufus::Scheduler.new
 
   scheduler.every '24h', first_in: '1s' do
